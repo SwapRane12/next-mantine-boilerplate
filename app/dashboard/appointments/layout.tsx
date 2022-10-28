f99b1de06933e285f7d82105d0d1d7b863e0d4c2
Swaprane12 <swaprr12@gmail.com>
2022-10-28 22:43:01 +0530

    fixed appointment layout

--- app/dashboard/appointments/layout.tsx
+++ app/dashboard/appointments/layout.tsx
@@ -10,25 +10,22 @@ export default function AppointmentLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<>
-			<div className=" flex flex-col gap-2">
-				<div className="flex flex-col  sm:flex-row justify-between">
-					<Title order={2} className="">Appointments</Title>
-					<div className="flex flex-col w-full sm:w-auto sm:flex-row flex-wrap gap-2">
-						<NextLink href="/dashboard/patients/new" passHref>
-							<Button className="w-full sm:w-auto " component="a" variant="filled" color="green">New
-								Appointment</Button>
-						</NextLink>
-						<Button className="w-full sm:w-auto " variant="filled" color="blue">All Appointments</Button>
-					</div>
-				
+		
+		<div className=" flex flex-col gap-2">
+			<div className="flex flex-col  sm:flex-row justify-between gap-2">
+				<Title order={2} className="">Appointments</Title>
+				<div className="flex flex-col w-full sm:w-auto sm:flex-row flex-wrap gap-2">
+					<NextLink href="/dashboard/patients/new" passHref>
+						<Button className="w-full sm:w-auto " component="a" variant="filled" color="green">New
+							Appointment</Button>
+					</NextLink>
+					<Button className="w-full sm:w-auto " variant="filled" color="blue">All Appointments</Button>
 				</div>
-			
 			
 			</div>
 			
 			{children}
-		</>
+		</div>
 	
 	
 	)
